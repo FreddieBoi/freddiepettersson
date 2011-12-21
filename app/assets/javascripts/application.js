b406c1fa6d090fdb59f99e5694b8a81c7c3f2492
Freddie <freddieboi@gmail.com>
2011-12-21 14:13:42 +0100

    Added Google +1 button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,13 @@
 //= require jquery_ujs
 //= require_self
 //= require_tree .
+
+<!--Google plus one button -->
+(function() {
+  var po = document.createElement('script');
+  po.type = 'text/javascript';
+  po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(po, s);
+})();
