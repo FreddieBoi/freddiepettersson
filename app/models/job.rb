53ca2569d6cd076c5bfea7d3c0115cec2caf1f54
Freddie <freddieboi@gmail.com>
2012-01-02 01:43:41 +0100

    Modified views of jobs and places

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,11 @@ class Job < ActiveRecord::Base
   validates :place, :presence => true
   validates :title, :presence => true
   validates :started_at, :presence => true
+  # Get the working period as properly formatted string (:started_at - :ended_at)
+  # Only showing year and month. The word "Present" is used if :ended_at is null.
+  def period
+    "#{self.started_at.strftime('%B %Y')} - #{self.ended_at.blank? ? "Present" : self.ended_at.strftime('%B %Y')}"
+  end
 end
 # == Schema Information
 #
